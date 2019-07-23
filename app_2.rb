48d709b4001d76dca54fdf44eb3f86fb080421b3
Frédéric <f.lugbull@gmail.com>
2019-07-23 23:28:43 +0200

    maj

---
+++ app_2.rb
@@ -0,0 +1,62 @@
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/game'
+require_relative 'lib/player'
+
+puts "-----------------------------------------------------"
+puts "| Bienvenue dans mon jeu 'Ils veulent tous ma pooooo!' |"
+puts "| Le but pour vous sera de survivre aux vilains Bots!  |"
+puts "-----------------------------------------------------"
+
+puts "comment t'appelles-tu, jeune Héro?"
+humanplayer_name = gets.chomp     # je stocke le nom du joueur dans une variable, que je rappelle la ligne d'après pour crééer mon Player 3 humain.
+
+Player3= HumanPlayer.new(humanplayer_name)
+Player1 = Player.new("Josiane")
+Player2 = Player.new("José")
+
+enemies= [Player1, Player2]
+
+while Player3.life_points>0 && (Player1.life_points>0 || Player2.life_points>0)   # Tant qu'aucun joueur n'est mort, continue la boucle while.
+  puts "Voici l'état de chaque joueur:"
+  puts Player1.show_state
+  puts Player2.show_state
+  puts Player3.show_state
+  puts "-------------------"    # Je rends le résultat plus lisible ici..
+  puts "Quelle action veux-tu effectuer ?
+        a - chercher une meilleure arme
+        s - chercher à se soigner 
+
+        attaquer un joueur en vue :
+        0 - Josiane a 10 points de vie
+        1 - José a 10 points de vie"
+
+Player3_choice= gets.chomp
+  if Player3_choice=="a"
+    Player3.search_weapon
+  elsif Player3_choice == "s"
+    Player3.search_health_pack
+  elsif Player3_choice == "0"
+    Player3.attacks(Player1)
+  else Player3_choice == "1"
+    Player3.attacks(Player2)
+  end
+
+  puts Player1.show_state
+  puts Player2.show_state
+  # if Player2.life_points<1 && Player1.life_points<1   # On sort tout de suite de la boucle si le Player2 est mort, pour éviter qu'il puisse attaqué alors qu'il est mort..
+  #   break
+  # end
+end
+
+binding.pry
+
+puts "La partie est finie"
+
+if Player3.life_points<1
+  puts "Loser ! Tu as perdu !"
+else 
+  puts "BRAVO ! TU AS GAGNE !"
+end
+
--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,4 @@
-
-# On définit notre classe Player ici.
+# On définit notre classe Player ici, et 2 objets.
 class Player   
   attr_accessor :name, :life_points
   @life_points=10
@@ -33,5 +32,61 @@ class Player
   def compute_damage
     return rand(1..6)   # Choix au hasard entre 1 et 6 points de dommages"
   end
+end
 
+
+
+
+
+class HumanPlayer < Player
+  attr_accessor :weapon_level   # Définition de la variable weapon_level qui indiquera sous forme d'integer le niveau de l'arme du HumanPlayer.
+
+  # On initialise avec 2 Objets.
+  def initialize(name)
+    @name = name
+    @life_points = 100
+    @weapon_level = 1
+  end
+
+  #On montre ici l'état du combattants (nb de points de vie restants)
+  def show_state
+    puts "#{@name} a #{@life_points} points de vie et une arme de niveau #{@weapon_level}"
+  end
+
+  def compute_damage
+    rand(1..6) * @weapon_level
+  end
+
+  # #Ici on va tomber sur une nouvelle arme (au hasard entre 1 et 6 de niveau)
+  def search_weapon
+    new_weapon_level = rand(1..6)
+    puts "Tu as trouvé une arme de niveau #{new_weapon_level}"
+      if new_weapon_level > @weapon_level
+        @weapon_level = new_weapon_level 
+        puts "Youhou, elle est meilleure que ton arme actuelle: tu la prends."
+      else
+        puts "M@*#$... elle n'est pas mieux que ton arme actuelle..."
+      end
+  end
+
+
+  # Méthode pour trouver des points de vie.
+  def search_health_pack
+    dice_roll = rand(1..6)
+    if dice_roll==1       # Le If ici va dire ce qu'il arrive en fonction du jeté de dé.
+      puts "Tu n'as rien trouvé."
+    elsif (dice_roll>1) && (dice_roll<6)  
+      @life_points = @life_points+50
+        if @life_points>100
+          @life_points = 100
+        end
+      puts "Bravo, tu as trouvé un pack de +50 points de vie!"
+    else dice_roll == 6
+      @life_points = @life_points+80
+      puts "Waow, tu as trouvé un pack de +80 points de vie !"
+        if @life_points>100
+          @life_points = 100
+        end
+    end
+  end
 end
