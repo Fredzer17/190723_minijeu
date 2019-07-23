64f2603849ba5b58a5efd67bfa25f63f170fbc42
Frédéric <f.lugbull@gmail.com>
2019-07-23 17:30:03 +0200

    1er push

---
+++ lib/player.rb
@@ -0,0 +1,37 @@
+
+# On définit notre classe Player ici.
+class Player   
+  attr_accessor :name, :life_points
+  @life_points=10
+
+  # On initialise avec 2 Objets.
+  def initialize(name)
+    @name = name
+    @life_points = 10
+  end
+
+  #On montre ici l'état du combattants (nb de points de vie restants)
+  def show_state
+    puts "#{@name} a #{@life_points} points de vie"
+  end
+
+  # def pour indiquer si le joueur est mort ou pas.
+  def gets_damage(damage_points)
+    @life_points = @life_points - damage_points
+      if @life_points <= 0       #Si le niveau de vie de l'objet est inférieur ou égal à zéro, le joueur est tué et on affiche un message.
+        puts "le joueur #{@name} a été tué!"   
+      end
+  end
+
+  def attacks(player_attacked)
+    puts "le joueur #{@name} attaque le joueur #{player_attacked.name}"   
+    damage_points = compute_damage
+    player_attacked.gets_damage(damage_points)    # On fait infliger les points de dommage au joueur attaqué.
+    puts "il lui inflige #{damage_points} points de dommages"
+  end
+
+  def compute_damage
+    return rand(1..6)   # Choix au hasard entre 1 et 6 points de dommages"
+  end
+
+end
