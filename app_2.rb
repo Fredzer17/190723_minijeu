8cddfca02b38e0018a2648935c12cfaa40d81c06
Frédéric <f.lugbull@gmail.com>
2019-07-23 23:42:14 +0200

    maj

--- app_2.rb
+++ app_2.rb
@@ -20,19 +20,19 @@ enemies= [Player1, Player2]
 
 while Player3.life_points>0 && (Player1.life_points>0 || Player2.life_points>0)   # Tant qu'aucun joueur n'est mort, continue la boucle while.
   puts "Voici l'état de chaque joueur:"
+  puts Player3.show_state
   puts Player1.show_state
   puts Player2.show_state
-  puts Player3.show_state
   puts "-------------------"    # Je rends le résultat plus lisible ici..
   puts "Quelle action veux-tu effectuer ?
         a - chercher une meilleure arme
         s - chercher à se soigner 
 
         attaquer un joueur en vue :
-        0 - Josiane a 10 points de vie
-        1 - José a 10 points de vie"
+        0 - Josiane a #{Player1.life_points} points de vie
+        1 - José a #{Player2.life_points} points de vie"
 
-Player3_choice= gets.chomp
+Player3_choice= gets.chomp    # Ici, j'indique l'action à réaliser selon l'input donné par le joueur humain.
   if Player3_choice=="a"
     Player3.search_weapon
   elsif Player3_choice == "s"
@@ -48,9 +48,12 @@ Player3_choice= gets.chomp
   # if Player2.life_points<1 && Player1.life_points<1   # On sort tout de suite de la boucle si le Player2 est mort, pour éviter qu'il puisse attaqué alors qu'il est mort..
   #   break
   # end
+  puts "Les autres joueurs t'attaquent!"   # Attaque de l'humain par les Bots, en utilisant l'array des ennemis.
+  enemies.each do |enemie|
+    enemie.attacks(Player3)
+  end
+  puts "----------------"
 end
-
-binding.pry
 
 puts "La partie est finie"
 
